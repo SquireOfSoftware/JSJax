9c9477219c8df0b9ea742899b24f489f13659741
Joseph Tran <fred.smoth@gmail.com>
2016-12-14 15:35:19 +1100

    Getting key presses recognised

--- js/sorting/game.js
+++ js/sorting/game.js
@@ -3,8 +3,114 @@
  */
 angular.module("webApp")
 .controller("gameCtrl", function($scope, $log, deckService) {
-    $scope.init = function() {
-        deckService.initialiseDeck();
+    $scope.cards = {
+        current: {},
+        next: {}
+    };
 
+    var pastCards = [];
+
+    $scope.pile = {
+        top: {},
+        left: {},
+        right: {},
+        bottom: {}
+    };
+
+    $scope.gameOver = false;
+
+    $scope.init = function() {
+        $scope.gameOver = false;
+        deckService.initialiseDeck();
+        setupTable();
+        setupSuits();
+    };
+
+    function setupTable() {
+        $scope.cards.current = deckService.drawCard();
+        $scope.cards.next = deckService.drawCard();
+    }
+
+    function setupSuits() {
+        var suits = [];
+        for(var x = 0; x < 4; x++) {
+            suits.push({
+                suit: getSuit(x),
+                points: 0,
+                cards: []
+            });
+        }
+        suits = shuffleSuits(suits);
+        $scope.pile = {
+            top: suits[0],
+            left: suits[1],
+            right: suits[2],
+            bottom: suits[3]
+        };
+        $log.debug($scope.pile);
+    }
+
+    function shuffleSuits (suitArray) {
+        for(var x = suitArray.length - 1; x > 0; x--) {
+            var position = Math.round(Math.random() * x, 0);
+            var temp = suitArray[position];
+            suitArray[position] = suitArray[x];
+            suitArray[x] = temp;
+        }
+        return suitArray;
+    }
+
+    function getSuit(number) {
+        switch(number) {
+            case 0: return "C";
+            case 1: return "D";
+            case 2: return "H";
+            case 3: return "S";
+            default: return " ";
+        }
+    }
+
+    function getNextCard() {
+        pastCards.push($scope.cards.current);
+        $scope.cards.current = $scope.cards.next;
+        $scope.cards.next = deckService.drawCard();
+    }
+
+    $scope.pressedKey = function(event) {
+        if (!$scope.gameOver)
+            processKeyPress(event.keyCode);
+    };
+
+    function processKeyPress(keyCode) {
+        $log.debug(event.keyCode);
+        switch(event.keyCode) {
+            case 37: // left
+                addCard($scope.pile.left);
+                break;
+            case 38: // up
+                addCard($scope.pile.top);
+                break;
+            case 39: // right
+                addCard($scope.pile.right);
+                break;
+            case 40: // down
+                addCard($scope.pile.bottom);
+                break;
+            default:
+        }
+    }
+
+    function addCard(pile) {
+        var currentCard = $scope.cards.current;
+        pile.cards.push(currentCard);
+        if (pile.suit === currentCard.suit) {
+            pile.points++;
+        }
+        getNextCard();
+
+        if($scope.cards.current === null){
+            $scope.gameOver = true;
+            $log.error("Game over")
+        }
     }
 });
