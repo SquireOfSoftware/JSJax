75e105bbd9f7e014f2b540bcfeac2e5322178ab7
Joseph Tran <fred.smoth@gmail.com>
2016-12-14 16:57:28 +1100

    Started optimising some of the useless functions

--- js/sorting/game.js
+++ js/sorting/game.js
@@ -3,7 +3,7 @@
  */
 var webApp = angular.module("webApp");
 
-webApp.controller("gameCtrl", function($scope, $log, deckService) {
+webApp.controller("gameCtrl", function($scope, $log, deckService, suitService) {
     $scope.cards = {
         current: {},
         next: {}
@@ -33,53 +33,13 @@ webApp.controller("gameCtrl", function($scope, $log, deckService) {
     }
 
     function setupSuits() {
-        var suits = [];
-        for(var x = 0; x < 4; x++) {
-            suits.push({
-                suit: getSuit(x),
-                symbol: getSuitSymbol(x),
-                points: 0,
-                cards: []
-            });
-        }
-        suits = shuffleSuits(suits);
+        var suits = suitService.setupSuits();
         $scope.pile = {
             top: suits[0],
             left: suits[1],
             right: suits[2],
             bottom: suits[3]
         };
-        $log.debug($scope.pile);
-    }
-
-    function shuffleSuits (suitArray) {
-        for(var x = suitArray.length - 1; x > 0; x--) {
-            var position = Math.round(Math.random() * x, 0);
-            var temp = suitArray[position];
-            suitArray[position] = suitArray[x];
-            suitArray[x] = temp;
-        }
-        return suitArray;
-    }
-
-    function getSuit(number) {
-        switch(number) {
-            case 0: return "C";
-            case 1: return "D";
-            case 2: return "H";
-            case 3: return "S";
-            default: return " ";
-        }
-    }
-
-    function getSuitSymbol(number) {
-        switch(number) {
-            case 0: return "Club";
-            case 1: return "Diamond";
-            case 2: return "Heart";
-            case 3: return "Spade";
-            default: return " ";
-        }
     }
 
     function getNextCard() {
@@ -132,10 +92,4 @@ webApp.controller("gameCtrl", function($scope, $log, deckService) {
             $log.error("Game over");
         }
     }
-});
-
-webApp.filter("unsafe", function($sce) {
-    return function(value) {
-        return $sce.trustAsHtml(value);
-    };
 });
